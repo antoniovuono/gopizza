ce3d2d0ce448ca3f7cfdabf840ee3a3db48a8e33
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-28 20:43:35 -0300

    theme configured and colors started to added to the project

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
+import theme from './src/global/theme';
 import Home from './src/screens/Home';
 
 const App: React.FC = () => {
-  return <Home />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ src/global/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary_background: '#B83341',
+    secondary_background2: '#E03F50',
+    third_background: '#F7F7F7',
+    shape: '#FFFFFF',
+    primary_title: '#572D31',
+    primary_text: '#7A6769',
+    shadow_details: '#D7D9E0',
+    shadow_separator: '#DCDCDC',
+    success: '#528F33',
+  },
+};
