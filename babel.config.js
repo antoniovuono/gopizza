6102d8584474f309281927f3f819e245fef73a63
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-30 10:00:23 -0300

    path mapping configured

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: './src',
+        extenions: ['.ts', '.tsx', '.js', '.json'],
+        alias: {
+          src: './src',
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
 };
