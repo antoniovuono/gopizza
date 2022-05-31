7820597f2c642646c76577d30aea4854bb499d52
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-05-30 21:52:18 -0300

    Show password action finalized!

--- src/screens/SignIn/components/PasswordInput/index.tsx
+++ src/screens/SignIn/components/PasswordInput/index.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import {TextInputProps} from 'react-native';
 import OpenEyeIcon from '@assets/icons/eye.svg';
+import ClosedEyeIcon from '@assets/icons/eye2.svg';
 
 import * as Styled from './styles';
 
 interface InputProps extends TextInputProps {
-  show_password?: () => void;
+  showPassword: boolean;
+  onPressPassword: () => void;
 }
 
-const PasswordInput: React.FC<InputProps> = ({...rest}) => {
+const PasswordInput: React.FC<InputProps> = ({
+  showPassword,
+  onPressPassword,
+  ...rest
+}) => {
   return (
     <Styled.Container>
       <Styled.TextInputCotnent
         {...rest}
         autoCapitalize="none"
         autoCorrect={false}
+        secureTextEntry={showPassword}
       />
 
-      <Styled.ButtonEye>
-        <OpenEyeIcon />
+      <Styled.ButtonEye onPress={onPressPassword}>
+        {showPassword ? <OpenEyeIcon /> : <ClosedEyeIcon />}
       </Styled.ButtonEye>
     </Styled.Container>
   );
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import LogoImage from '@assets/images/brand.png';
 
 import * as Styled from './styles';
@@ -6,6 +6,12 @@ import SignInInputs from './components/SignInInputs';
 import PasswordInput from './components/PasswordInput';
 
 const SignIn: React.FC = () => {
+  const [showPassword, setShowPassword] = useState(true);
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <Styled.Container>
       <Styled.Content>
@@ -13,7 +19,11 @@ const SignIn: React.FC = () => {
         <Styled.Title>Login</Styled.Title>
 
         <SignInInputs placeholder="E-mail" />
-        <PasswordInput placeholder="Password" />
+        <PasswordInput
+          placeholder="Password"
+          showPassword={showPassword}
+          onPressPassword={handleShowPassword}
+        />
       </Styled.Content>
     </Styled.Container>
   );
