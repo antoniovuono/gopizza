2ded45b858a7dc66d0fee98da3aa1f7c9fa15204
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-06-02 10:16:36 -0300

    Register product created

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import Home from '@screens/Home';
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import theme from './src/global/theme';
 // import SignIn from './src/screens/SignIn';
-
+import RegisterProduct from '@screens/RegisterProduct';
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <RegisterProduct />
     </ThemeProvider>
   );
 };
