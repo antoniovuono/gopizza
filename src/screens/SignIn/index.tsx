00047ea23c4e4d3eefe9b57e324b631dd4e76590
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-04-08 12:08:04 -0300

    input placed from sign in screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,25 @@
+import { Input } from "@components/Input";
 import React from "react";
 
 import { Container } from "./styles";
 
 export const SignIn = () => {
-    return <Container></Container>;
+    return (
+        <Container>
+            <Input
+                placeholder="E-mail"
+                type="secondary"
+                autoCorrect={false}
+                autoCapitalize="none"
+            />
+
+            <Input
+                placeholder="Senha"
+                type="secondary"
+                autoCorrect={false}
+                autoCapitalize="none"
+                secureTextEntry
+            />
+        </Container>
+    );
 };
