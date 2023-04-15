6e4443335c5692ca16d79b4eb78b5533f880d7ef
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-04-15 13:57:54 -0300

    Interface order was created

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,32 +1,73 @@
+import { Button } from "@components/Button";
 import { ButtonBack } from "@components/ButtonBack";
+import { Input } from "@components/Input";
 import { RadioButton } from "@components/RadioButton";
 import { PIZZA_TYPES } from "@utils/pizzaTypes";
 import React, { useState } from "react";
 import { Platform } from "react-native";
 
-import { Container, Header, Photo, Sizes } from "./styles";
+import {
+    Container,
+    ContentScroll,
+    Form,
+    FormRow,
+    Header,
+    InputGroup,
+    Label,
+    Photo,
+    Price,
+    Sizes,
+    Title,
+} from "./styles";
 
 export const Order = () => {
     const [size, setSize] = useState("");
 
     return (
         <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-            <Header>
-                <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-            </Header>
-
-            <Photo source={{ uri: "https://github.com/antoniovuono.png" }} />
-
-            <Sizes>
-                {PIZZA_TYPES.map((item) => (
-                    <RadioButton
-                        key={item.id}
-                        selected={item.id === size}
-                        title={item.name}
-                        onPress={() => setSize(item.id)}
+            <ContentScroll>
+                <Header>
+                    <ButtonBack
+                        onPress={() => {}}
+                        style={{ marginBottom: 108 }}
                     />
-                ))}
-            </Sizes>
+                </Header>
+
+                <Photo
+                    source={{ uri: "https://github.com/antoniovuono.png" }}
+                />
+
+                <Form>
+                    <Title>Nome da Pizza</Title>
+                    <Label>Selecione um tamanho</Label>
+                    <Sizes>
+                        {PIZZA_TYPES.map((item) => (
+                            <RadioButton
+                                key={item.id}
+                                selected={item.id === size}
+                                title={item.name}
+                                onPress={() => setSize(item.id)}
+                            />
+                        ))}
+                    </Sizes>
+
+                    <FormRow>
+                        <InputGroup>
+                            <Label>Número da mesa</Label>
+                            <Input keyboardType="numeric" />
+                        </InputGroup>
+
+                        <InputGroup>
+                            <Label>Quantidade</Label>
+                            <Input keyboardType="numeric" />
+                        </InputGroup>
+                    </FormRow>
+
+                    <Price>Valor de R$ 00,00</Price>
+
+                    <Button title="Confirmar pedido" />
+                </Form>
+            </ContentScroll>
         </Container>
     );
 };
