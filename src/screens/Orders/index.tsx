cbe599c96240148762df8855bb5e940d62a4d092
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2023-04-15 14:07:31 -0300

    orders interface header finalized

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Header, Title } from "./styles";
 
 export const Orders = () => {
-    return <Container></Container>;
+    return (
+        <Container>
+            <Header>
+                <Title>Pedidos feitos</Title>
+            </Header>
+        </Container>
+    );
 };
--- src/screens/Orders/styles.ts
+++ src/screens/Orders/styles.ts
@@ -1,5 +1,24 @@
+import { LinearGradient } from "expo-linear-gradient";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { css } from "styled-components";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
+`;
+
+export const Header = styled(LinearGradient).attrs(({ theme }) => ({
+    colors: theme.COLORS.GRADIENT,
+}))`
+    padding: ${getStatusBarHeight() + 33}px 0 33px;
+`;
+
+export const Title = styled.Text`
+    font-size: 24px;
+    text-align: center;
+
+    ${({ theme }) => css`
+        font-family: ${theme.FONTS.TITLE};
+        color: ${theme.COLORS.TITLE};
+    `}
 `;
